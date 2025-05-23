1d34ce8920c09c3d849152826b41e3db934e8b86
use-the-fork <23747916+use-the-fork@users.noreply.github.com>
2025-05-23 14:11:24 -0400

    feat: add configuration for various shell tools and utilities

---
+++ src/dots/home/tools/atuin.nix
@@ -0,0 +1,19 @@
+{osConfig, ...}: let
+  inherit (osConfig) modules;
+  dev = modules.device;
+in {
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      # https://docs.atuin.sh/configuration/config/
+      search_mode = "fuzzy";
+      filter_mode = "global";
+
+      history_filter = [
+        "^ls"
+      ];
+    };
+  };
+}
---
+++ src/dots/home/tools/default.nix
@@ -0,0 +1,17 @@
+{
+  imports = [
+    # ./bin
+    # ./git
+    
+    ./atuin.nix
+    ./bat.nix
+    ./dircolors.nix
+    ./direnv.nix
+    ./editorconfig.nix
+    ./eza.nix
+    ./fzf.nix
+    ./ripgrep.nix
+    ./zoxide.nix
+
+  ];
+}
