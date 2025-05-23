69c5fb2e70441fdabd46cdff9c1861543e8fcfd6
use-the-fork <23747916+use-the-fork@users.noreply.github.com>
2025-05-23 16:13:27 -0400

    feat: add support for custom config.nix path and update flake structure

--- src/dots/flake.nix
+++ src/dots/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Snowblower devcontainer";
+  description = "Snow Blower";
 
   inputs = {
     # global, so they can be `.follow`ed
@@ -14,35 +14,53 @@
 
     # Powered by
     flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    home-manager,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      username = "USER";
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      legacyPackages = {
-        homeConfigurations = {
-          "${username}" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-              {
-                home = {
-                  username = "${username}";
-                  homeDirectory = "/home/${username}";
-                };
-              }
-              ./home
-            ];
-          };
-        };
-      };
-    });
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      # Systems for which attributes of perSystem will be built. As
+      # a rule of thumb, only systems provided by available hosts
+      # should go in this list. More systems will increase evaluation
+      # duration.
+      systems = import inputs.systems;
+
+      imports = [
+        ./flake # Parts of the flake that are used to construct the final flake.
+      ];
+    };
+
+  # outputs = {
+  #   nixpkgs,
+  #   flake-utils,
+  #   home-manager,
+  #   ...
+  # }:
+  #   flake-utils.lib.eachDefaultSystem (system: let
+  #     username = "USER";
+  #     pkgs = import nixpkgs {inherit system;};
+  #   in {
+  #     legacyPackages = {
+  #       homeConfigurations = {
+  #         "${username}" = home-manager.lib.homeManagerConfiguration {
+  #           inherit pkgs;
+  #           modules = [
+  #             {
+  #               home = {
+  #                 username = "${username}";
+  #                 homeDirectory = "/home/${username}";
+  #               };
+  #             }
+  #             ./home
+  #           ];
+  #         };
+  #       };
+  #     };
+  #   });
 
   nixConfig = {
     extra-trusted-public-keys = [
